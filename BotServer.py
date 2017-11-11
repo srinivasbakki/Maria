88d56e41e9862d9bbe1cb35d4cb454c68017a8c1
srinivasbakki <srinivas.bakki@gmail.com>
2017-11-11 11:47:03 +0530

    print - return

--- BotServer.py
+++ BotServer.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-    print "Handling verification."
+    return"Handling verification."
     
 if __name__ == '__main__':
     app.run()
